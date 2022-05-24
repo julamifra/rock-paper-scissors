ea7049716e9fe06d3f0e7fba1dbfa02db179baf0
julamifra <julamifra@gmail.com>
2022-05-24 18:42:36 +0000

    Add main function

---
+++ run.py
@@ -0,0 +1,21 @@
+
+
+def main():
+    '''
+    Inialize the game to start playing.
+    Run all program functions
+    '''
+    print('-------------------------------')
+    print('Welcome to ROCK-PAPER-SICCORS!!')
+    print('In order to play, use the following letter:')
+    print('r: rock')
+    print('p: paper')
+    print('s: scissor')
+    print('GOOD LUCK!')
+    print('-------------------------------')
+    player_name = input('Please enter your name: ')
+    print(player_name)
+   
+
+main()
+
